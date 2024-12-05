cdb28265f17686f95084f21ddf981c1173db14da
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-12-05 09:00:18 +0100

    Added model builder comments. GitIgnore changed (double entry).

--- Bookstore.API/Data/ApplicationDbContext.cs
+++ Bookstore.API/Data/ApplicationDbContext.cs
@@ -5,38 +5,50 @@ namespace Bookstore.API.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        
-        
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<StorageLocation> StoraLocations { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder options) => 
             options.UseSqlServer(Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") ?? throw new NullReferenceException("Database connection string not loaded"));
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Book 
+            // Book table PK
             modelBuilder.Entity<Book>()
                 .HasKey(b => b.ISBN);
 
+            // Book - Author relation
             modelBuilder.Entity<Book>()
                 .HasOne(b => b.Author)
                 .WithMany()
                 .HasForeignKey(b => b.AuthorId);
 
+            // Book - Language relation
             modelBuilder.Entity<Book>()
                 .HasOne(b => b.Language)
                 .WithMany()
                 .HasForeignKey(b => b.LanguageId);
 
+            // Book - Publisher relation
             modelBuilder.Entity<Book>()
                 .HasOne(b => b.Publisher)
                 .WithMany()
                 .HasForeignKey(b => b.PublisherId);
 
+            // Book - Ratings relation
             modelBuilder.Entity<Book>()
                 .HasMany(b => b.Ratings)
                 .WithOne()
                 .HasForeignKey(r => r.Id);
 
+            // Book - StorageLocation relation
+            // EF setup to manage join table
             modelBuilder.Entity<Book>()
                 .HasMany(b => b.StorageLocations)
                 .WithMany(sl => sl.Books)
